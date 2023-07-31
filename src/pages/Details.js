2476be728c4da5cad4cebb57814910f332e2161b
KilonzoJames <jay.kilonzo@gmail.com>
2023-07-31 16:44:01 +0300

    Added Details to Details Card

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -10,25 +10,30 @@ function Details({botsArray, setBotsArray,favouriteBots,setFavouriteBots}) {
     const isBotAlreadyAdded = favouriteBots.some((bot) => bot.id === botId);
     !isBotAlreadyAdded?
     setFavouriteBots([...favouriteBots, draftedBot]):console.log('Already in favourites');
+    event.target.classList.add("clicked");
   }
   
   return (
     <div>
       <h1>DETAILS</h1>
-      <nav id="sidebar">
-      <NavLink exact to='../'>Home</NavLink>
-      </nav>
       <div className='card col-2 bg-secondary' key={bot.id}>
       <img src={bot.avatar_url} className="card-img-top" alt="Loading..."/>
         <div className="card-body" >
-            <h3 className="card-title">{bot.name}</h3>
-            <p className="card-text">{bot.catchphrase}</p>
+            <h3 className="card-title">Name: {bot.name}</h3>
+            <p className="card-text"><strong>Catchphrase</strong>: <br/>{bot.catchphrase}</p>
+            <h3>Class: {bot.bot_class}&#9992;</h3>
             <h4>&#128147;{bot.health} &#128737;{bot.armor} &#9889;{bot.damage}</h4>
             <button 
             onClick={addToFavourites}
+            className="btn btn-danger"
+            id={bot.id}
+            >  Enlist  
+            </button>
+            <br/>
+            <button 
             className="btn btn-info"
             id={bot.id}
-            >Favourites
+            ><NavLink exact to='../'>Go Back</NavLink>
             </button>
         </div>
     </div>
